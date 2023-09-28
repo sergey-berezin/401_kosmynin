aa4e810ff5ee8fde10ef2438a9dd3e5238f46410
Stas <kosmynin_02@mail.ru>
2023-09-28 23:13:45 +0300

    first commit(not final)

---
+++ Bert/Bert.App/Program.cs
@@ -0,0 +1,39 @@
+﻿using Bert.Nuget;
+
+namespace Bert.App;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    { 
+        var bertModelConnection = BertModelConnection.GetBertModelConnection();
+        await bertModelConnection.CreateOrDownloadAsync("https://storage.yandexcloud.net/dotnet4/bert-large-uncased-whole-word-masking-finetuned-squad.onnx");
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+        
+        string filePath = args[0];
+        string text = await File.ReadAllTextAsync(filePath, token);
+        
+        var answerText = bertModelConnection.ExecuteAsync(text, token);
+        Console.WriteLine(answerText);
+        while (!token.IsCancellationRequested)
+        {
+            var prompt = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(prompt))
+            {
+                break;
+            }
+
+            try
+            {
+                var answer = bertModelConnection.ExecuteAsync(prompt, token);
+                Console.WriteLine(answer);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
